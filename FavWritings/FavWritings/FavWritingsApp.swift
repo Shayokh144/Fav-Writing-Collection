52eb5f8216ccae3ca853104092c4417a73c1184e
Shayokh144 <shayokhbd144@gmail.com@gmail.com>
2024-05-07 21:49:55 +0700

    Initial app added

---
+++ FavWritings/FavWritings/FavWritingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavWritingsApp.swift
+//  FavWritings
+//
+//  Created by Taher's nimble macbook on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FavWritingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
